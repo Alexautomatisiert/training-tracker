3f661edd803079502781ae045513882ef247c47d
Alexautomatisiert <alexanderschwan1@gmail.com>
2026-06-21 08:48:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'training-v33';
+const CACHE = 'training-v34';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', e => {
