2dd5e61aa118238e7494542a7c25fd5d5204928b
Alexautomatisiert <alexanderschwan1@gmail.com>
2026-07-19 17:56:25 +0200

    Free Workout: Vereinfachter Übungs-Flow + Katalog-Schnellauswahl
    - Übung hinzufügen zeigt gespeicherte Übungen als Schnellauswahl-Chips
    - Gewicht/Sätze/Reps aus Maske entfernt (kommt im Training)
    - BW-Übungen bekommen automatisch aktuelles Bodyweight
    - EMOM als eigener Block-Typ (3 Buttons: Sätze/EMOM/Zirkel)
    - v7.7, cache training-v45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'training-v44';
+const CACHE = 'training-v45';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', e => {
