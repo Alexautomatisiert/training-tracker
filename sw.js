8254244b337b57097034cf209c3fefa2254d4b4e
Alexautomatisiert <alexanderschwan1@gmail.com>
2026-07-19 19:15:24 +0200

    v8.2: Timer box fix, free workout edit/delete

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'training-v48';
+const CACHE = 'training-v49';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', e => {
