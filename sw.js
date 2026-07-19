a230df5e69d81684b5a3394d08a8666791c069c3
Alexautomatisiert <alexanderschwan1@gmail.com>
2026-07-19 22:20:52 +0200

    Mesozyklus-Übersicht, Cross-Meso-Referenzwerte und erweiterte Übungshistorie

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'training-v50';
+const CACHE = 'training-v51';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', e => {
